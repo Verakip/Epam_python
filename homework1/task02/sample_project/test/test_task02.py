19c9bf303504956432e6b654ff9658c6d9f05e2d
vera <Vera_Kipiatkova@epam.com>
2021-03-31 13:23:57 +0300

    test commit

--- homework1/task02/sample_project/test/test_task02.py
+++ homework1/task02/sample_project/test/test_task02.py
@@ -1,6 +1,22 @@
+import pytest
 from homework1.task02 import check_fibonacci
 
 
 
-def test_main():
-    assert True
+
+@pytest.mark.parametrize(
+    ["value", "expected_result"],
+    [
+        ([6,4,6], False),
+        ([1,2], True),
+        ([8,2,1], True),
+        ([9], False),
+        ([0], True),
+        ([-1], False)
+    ],
+)
+
+def test_check_fibonacci(value: Sequence, expected_result: bool):
+    actual_result = check_fibonacci(value)
+
+    assert actual_result == expected_result
