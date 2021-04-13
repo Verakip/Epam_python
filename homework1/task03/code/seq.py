287403fedd1b91dadcee13e9d06438b4b03323b8
vera <Vera_Kipiatkova@epam.com>
2021-04-13 18:26:13 +0300

    task03

--- homework1/task03/code/seq.py
+++ homework1/task03/code/seq.py
@@ -3,12 +3,12 @@ from typing import Tuple
 
 
 def find_maximum_and_minimum(file_name: str) -> Tuple[int, int]:
-    mi = 10000000
-    ma = -10000000
     with open(file_name) as fi:
+        mi = int(fi.readline())
+        ma = int(fi.readline())
         for line in fi:
-            if int(line) < mi:
+            if int(line) <= mi:
                 mi = int(line)
-            elif int(line) > ma:
+            elif int(line) >= ma:
                 ma = int(line)
     return(mi, ma)
